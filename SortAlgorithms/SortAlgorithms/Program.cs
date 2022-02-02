da62087627e1088ee44150b94a0a32af39941985
roman-kart <81640683+roman-kart@users.noreply.github.com>
2022-02-02 23:34:28 +0300

    Добавил примеры алгоритмов поиска: простого перебора и бинарного

--- SortAlgorithms/SortAlgorithms/Program.cs
+++ SortAlgorithms/SortAlgorithms/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
-namespace SortAlgorithms
+namespace SortAndSearchAlgorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var itemsSortedList = new List<int>();
+            for (int i = 0; i < 214700000; i++)
+            {
+                itemsSortedList.Add(i);
+            }
+            var itemsSorted = itemsSortedList.ToArray();
+
+            var timer = new Stopwatch();
+
+
+            SearchAlgorithmTimer.TimeOfExecution<int>(SearchAlgorithms.SimpleSearch, itemsSorted, 234900);
+            SearchAlgorithmTimer.TimeOfExecution<int>(SearchAlgorithms.BinarySeacrhAlgorithm, itemsSorted, 234900);
         }
     }
 }
---
+++ SortAlgorithms/SortAlgorithms/SearchAlgorithms.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortAndSearchAlgorithms
+{
+    public class SearchAlgorithms
+    {
+        #region Simple Search Algorithm
+        ///<summary>
+        ///<para>
+        /// Search element in sequence by element.Equals(searchable). 
+        ///</para>
+        ///<para>
+        /// If sequence contains searchable element, return index,
+        /// Else return -1.
+        ///</para>
+        ///</summary>
+        public static int SimpleSearch<type>(type[] items, type item) where type : IEquatable<type>
+        {
+            var length = items.Length;
+            int i = 0;
+            while (i < length && !(items[i].Equals(item)))
+            {
+                i++;
+            }
+            if (i < length)
+            {
+                return i;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        #endregion
+        /// <summary>
+        /// <para>
+        ///     Search element in sequence by binary search.
+        /// </para>
+        /// <para>
+        ///     If searchable item is in sequence, returns index,
+        ///     else returns -1.
+        /// </para>
+        /// </summary>
+        /// <typeparam name="type"></typeparam>
+        /// <param name="items"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        #region Binary Search Algorithm
+        public static int BinarySeacrhAlgorithm<type>(type[] items, type item) where type : IEquatable<type>, IComparable<type>
+        {
+            int m = 0;
+            int left = 0;
+            int right = items.Length - 1;
+            do
+            {
+                m = (left + right) / 2;
+                // если искомый элемент меньше, чем элемент посередине
+                if (item.CompareTo(items[m]) > 0)
+                {
+                    left = m + 1;
+                }
+                else
+                {
+                    right = m - 1;
+                }
+            } while ((!items[m].Equals(item)) && left <= right);
+            if (items[m].Equals(item))
+            {
+                return m;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        #endregion
+    }
+}
