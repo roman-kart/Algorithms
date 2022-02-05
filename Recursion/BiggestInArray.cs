15f6c453ac1da7194bc0bbdf445ad72302446392
roman-kart <romankartasheff@gmail.com>
2022-02-05 22:28:03 +0300

    Добавил реализацию алгоритма поиска наибольшего числа в массиве при помощи рекурсии

---
+++ Recursion/BiggestInArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    internal class BiggestInArray
+    {
+        public static int Get(int[] elements)
+        {
+            return FindBiggest(elements, 0, 0);
+        }
+        private static int FindBiggest(int[] elements, int biggestIndex, int currentIndex)
+        {
+            if (elements.Length == 0)
+            {
+                return 0;
+            }
+            if (currentIndex >= elements.Length)
+            {
+                return elements[biggestIndex];
+            }
+            else if (elements[biggestIndex] < elements[currentIndex])
+            {
+                return FindBiggest(elements, currentIndex, currentIndex + 1);
+            }
+            else
+            {
+                return FindBiggest(elements, biggestIndex, currentIndex + 1);
+            }
+        }
+    }
+}
--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -2,3 +2,4 @@
 global::Recursion.Field.PrintMinSquare(14, 21);
 Console.WriteLine(global::Recursion.Sum.GetSum(new int[] { 1, 2, 3}));
 Console.WriteLine(global::Recursion.CountOfElements.Get<int>(new int[] {1, 2, 3, 4, 5}));
+Console.WriteLine(global::Recursion.BiggestInArray.Get(new int[] {1, 2, 3, 4, 5, 55}));
